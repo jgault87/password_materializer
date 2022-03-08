cbbe2a174b6074312a04f573e2430d5eae7ce129
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-03-08 15:54:41 -0700

    added generator functions for symbols, numbers, upper and lowercase letters using javascript char table gen

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,41 @@
+// pseudocode
+// 8 - 128 chars
+// lowercase
+//  upprcase
+//  numbers
+//  special characters
+
+
+
+const randomFunction = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbols: getRandomSymbol,
+};
+
+//functions for character generation.
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.?:;~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol());
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
